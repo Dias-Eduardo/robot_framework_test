224a63bea8fb415f75635d411b0fc8b6b2d309b7
root <root@localhost.localdomain>
2022-12-16 14:33:27 -0300

    teste

--- commit.sh
+++ commit.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
 sudo git add *
+echo -e "Para a conseguir realizar o commit utilize o token a seguir no campo de senha"
+echo -e  "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"
 
 sudo git commit -m "$1"
 
